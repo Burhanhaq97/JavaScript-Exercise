315c2d0d63b4f579ffe207500bec367d5b69984b
Burhan <burhanhaq789@gmail.com>
2023-04-02 20:05:36 +0500

    challenge 1 solution

--- Return-the-Sum-of-Two-Numbers.js
+++ Return-the-Sum-of-Two-Numbers.js
@@ -9,10 +9,10 @@
 // Notes
 // Don't forget to return the result.
 
-
-
 // solution
-const addition = (num1, num2) =>  num1 + num2;
+const addition = (num1, num2) => {
+  return num1 + num2;
+};
 
 console.log(addition(3, 2));
 console.log(addition(-3, -2));
