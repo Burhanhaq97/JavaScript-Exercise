d05caf7e1a96f929c4c01ab18910af9a9627c5af
Burhan <burhanhaq789@gmail.com>
2023-04-02 20:00:19 +0500

    challenge 1 solution

---
+++ Return-the-Sum-of-Two-Numbers.js
@@ -0,0 +1,19 @@
+// Return the Sum of Two Numbers
+// Create a function that takes two numbers as arguments and returns their sum.
+
+// Examples
+// addition(3, 2) ➞ 5
+// addition(-3, -6) ➞ -9
+// addition(7, 3) ➞ 10
+
+// Notes
+// Don't forget to return the result.
+
+
+
+// solution
+const addition = (num1, num2) =>  num1 + num2;
+
+console.log(addition(3, 2));
+console.log(addition(-3, -2));
+console.log(addition(7, 3));
