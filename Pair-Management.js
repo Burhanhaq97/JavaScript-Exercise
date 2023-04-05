0f4db8af6c004f13dce1ad603a881ebc50e33a8b
Burhan <burhanhaq789@gmail.com>
2023-04-05 10:59:09 +0500

    challenge 7 solution

---
+++ Pair-Management.js
@@ -0,0 +1,18 @@
+/*
+Pair Management
+Create a function that takes two numbers as arguments and returns an array which contains these two arguments.
+
+Examples:
+makePair(1, 2) ➞ [1, 2]
+makePair(51, 21) ➞ [51, 21]
+makePair(512124, 215) ➞ [512124, 215]
+ */
+
+// solution
+const makePair = (val1, val2) => {
+  return [val1, val2];
+};
+
+console.log(makePair(1, 2));
+console.log(makePair(51, 21));
+console.log(makePair(512124, 215));
